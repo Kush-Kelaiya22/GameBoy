4dc5d5d4a5af03be61f5fd409454f42888a92a20
Kush Kelaiya <kelaiyakush@gmail.com>
2024-05-28 14:57:39 +0530

    Basic project setup

---
+++ inc/inc.h
@@ -0,0 +1,18 @@
+#ifndef __INC__
+#define __INC__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	typedef unsigned char Byte;
+	typedef unsigned short Word;
+	typedef unsigned int ui32;
+	typedef signed char SByte;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __INC__
