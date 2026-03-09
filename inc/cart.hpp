59f63cbccf0233b556b2578e87cc9eb49b61b53e
Kush Kelaiya <kelaiyakush@gmail.com>
2026-03-10 01:27:41 +0530

    Implemented MBC classes for bettery cartridge memory management

--- inc/cart.hpp
+++ inc/cart.hpp
@@ -6,10 +6,59 @@
 
 namespace gameboy
 {
+	class MBC_base
+	{
+	protected:
+		Byte* ROM;
+		Byte* RAM;
+	public:
+		MBC_base(Byte* rom, Byte* ram = nullptr);
+		virtual Byte read(Word Addr) = 0;
+		virtual void write(Word Addr, Byte data) = 0;
+	};
+	class MBC0 : public MBC_base
+	{
+	public:
+		MBC0(Byte* rom, Byte* ram);
+		Byte read(Word Addr) override;
+		void write(Word Addr, Byte data) override;
+	};
+	class MBC1 : public MBC_base
+	{
+	private:
+	public:
+	};
+	class MBC2 : public MBC_base
+	{
+	private:
+		struct
+		{
+			Byte low : 4;
+			Byte high : 4;
+		} sram[256];
+		Byte* rom_bank_ptr;
+		Byte bank_number : 4;
+		Byte ram_enable : 1;
+	public:
+		MBC2(Byte* rom, Byte* ram);
+		Byte read(Word Addr) override;
+		void write(Word Addr, Byte data) override;
+	};
+	class MBC3 : public MBC_base
+	{
+	};
+	class MBC5 : public MBC_base
+	{
+	};
+	class MBC6 : public MBC_base
+	{
+	};
+	class MBC7 : public MBC_base
+	{
+	};
 	class CART
 	{
 	private:
-		struct FILE;
 		static constexpr Byte ram_lut[] = {
 			[0x00] = 0,
 			[0x01] = 0,
@@ -29,41 +78,38 @@ namespace gameboy
 			[0x07] = 7,
 			[0x08] = 8
 		};
-		// static constexpr bool ram_enable_lut[] = {
-		// 	[0x00] = false,
-		// 	[0x01] = false,
-		// 	[0x02] = true,
-		// 	[0x03] = true,
-		// 	[0x05] = false,
-		// 	[0x06] = false,
-		// 	[0x08] = true,
-		// 	[0x09] = true,
-		// 	[0x0B] = false,
-		// 	[0x0C] = true,
-		// 	[0x0D] = true,
-		// 	[0x0F] = false,
-		// 	[0x10] = true,
-		// 	[0x11] = false,
-		// 	[0x12] = true,
-		// 	[0x13] = true,
-		// 	[0x19] = false,
-		// 	[0x1A] = true,
-		// 	[0x1B] = true,
-		// 	[0x1C] = false,
-		// 	[0x1D] = true,
-		// 	[0x1E] = true,
-		// 	[0x20] = false,
-		// 	[0x22] = true,
-		// 	[0xFE] = false,
-		// 	[0xFF] = true
+		// static constexpr Byte ram_enable_lut[] = {
+		// 	[0x00] = 0,
+		// 	[0x01] = 0,
+		// 	[0x02] = 1,
+		// 	[0x03] = 1,
+		// 	[0x05] = 0,
+		// 	[0x06] = 0,
+		// 	[0x08] = 1,
+		// 	[0x09] = 1,
+		// 	[0x0B] = 0,
+		// 	[0x0C] = 1,
+		// 	[0x0D] = 1,
+		// 	[0x0F] = 0,
+		// 	[0x10] = 1,
+		// 	[0x11] = 0,
+		// 	[0x12] = 1,
+		// 	[0x13] = 1,
+		// 	[0x19] = 0,
+		// 	[0x1A] = 1,
+		// 	[0x1B] = 1,
+		// 	[0x1C] = 0,
+		// 	[0x1D] = 1,
+		// 	[0x1E] = 1,
+		// 	[0x20] = 0,
+		// 	[0x22] = 1,
+		// 	[0xFE] = 0,
+		// 	[0xFF] = 1
 		// };
 		std::string ROM_NAME;
 		Byte* ROM;
 		Byte* RAM;
-		Byte ROM_BANK_NUMBER, ROM_BANK_SELECT[2];
-		Byte RAM_BANK_NUMBER, RAM_BANK_SELECT[2];
-		bool MBC1_Mode;
-		bool RAM_BANK_ENABLE[2];
+		MBC_base* mbc;
 	public:
 		CART(std::string filename);
 		~CART();
--- src/cart.cpp
+++ src/cart.cpp
@@ -4,15 +4,64 @@
 
 namespace gameboy
 {
+	MBC_base::MBC_base(Byte* rom, Byte* ram) : ROM(rom), RAM(ram)
+	{
+	}
+
+	MBC0::MBC0(Byte* rom, Byte* ram) : MBC_base(rom, ram)
+	{
+	}
+
+	Byte MBC0::read(Word Addr)
+	{
+		Byte data;
+		if (Addr >= 0x0000 and Addr <= 0x7FFF) data = this->ROM[Addr];
+		else if (Addr >= 0xA000 and Addr <= 0xBFFF) data = this->RAM[Addr & 0x1FFF];
+		else throw "Invalid address read";
+		return data;
+	}
+
+	void MBC0::write(Word Addr, Byte data)
+	{
+		if (Addr >= 0xA000 and Addr <= 0xBFFF) this->RAM[Addr & 0x1FFF] = data;
+	}
+
 	CART::CART(std::string filename)
 	{
+		this->ROM_NAME = filename;
 		std::ifstream file(filename, std::ios::binary);
 		if (!file)
 		{
-			std::cerr << "Error: Could not open ROM file." << '\n';
+			std::cerr << "Error: Could not open ROM file." << this->ROM_NAME << '\n';
 			exit(1);
 		}
-		this->RAM = new Byte[0x2000 * (1 << this->ram_lut[0x00])];
+		this->ROM = new Byte[0x200];
+		file.read(reinterpret_cast<char*>(this->ROM), 0x200);
+		file.seekg(0, std::ios::beg);
+	}
+
+	MBC2::MBC2(Byte* rom, Byte* ram) : MBC_base(rom, ram)
+	{
+		this->rom_bank_ptr = (this->ROM + 0x4000);
+	}
+
+	Byte MBC2::read(Word Addr)
+	{
+		Byte data = 0;
+		if (Addr >= 0x0000 and Addr <= 0x3FFF) data = this->ROM[Addr];
+		else if (Addr >= 0x4000 and Addr <= 0x7FFF) data = this->rom_bank_ptr[Addr & 0x3FFF];
+		return data;
+	}
+
+	void MBC2::write(Word Addr, Byte Data)
+	{
+		if (Addr & 0x0100)
+		{
+			bank_number = Data & 0x0F;
+			if (!bank_number) bank_number = 1;
+			this->rom_bank_ptr = (this->ROM + (bank_number * 0x4000));
+		}
+		else ram_enable = Data & 0x01;
 	}
 
 	CART::~CART()
@@ -23,25 +72,11 @@ namespace gameboy
 
 	Byte CART::read(Word Addr)
 	{
-		if (Addr < 0x8000)
-		{
-			return this->ROM[Addr];
-		}
-		else if (Addr >= 0xA000 && Addr <= 0xBFFF)
-		{
-			return this->RAM[Addr & 0x1FFF];
-		}
-		else
-		{
-			return 0;
-		}
+		return this->mbc->read(Addr);
 	}
 
 	void CART::write(Word Addr, Byte data)
 	{
-		if (Addr >= 0xA000 && Addr <= 0xBFFF)
-		{
-			this->RAM[Addr & 0x1FFF] = data;
-		}
+		this->mbc->write(Addr, data);
 	}
 }
